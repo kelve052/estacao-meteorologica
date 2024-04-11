8b29ae471079f42f3bbad1116421fe1d0a687701
Kamila Fortunato <kamilap391@gmail.com>
2024-04-10 21:48:37 -0400

    Criando rota cadastrar Estação: primeiro passo verificar se foi passado algum parametro

--- src/controllers/estacaoController.js
+++ src/controllers/estacaoController.js
@@ -10,16 +10,52 @@ class Estacao {
   static listar = async (req, res) => {
     try {
       const response = await prisma.estacao.findMany()
-      res.status(200).json({response: response})
+      res.status(200).json({ response: response })
     } catch (error) {
-      res.status(400).json({ 
-        error: true, 
-        code: 400, 
+      res.status(400).json({
+        error: true,
+        code: 400,
         message: error.message
       }
-)
+      )
     }
   }
+
+  //rota kamila cadastrar estacao
+
+  static cadastrarEstacao = async (req, res) => {
+    try {
+      const { nome, endereco, latitude, longitude, ip, status, usuario_id } = req.body;
+
+      const erros = [];
+
+      if (!nome) {
+        erros.push({ error: true, code: 400, message: "Nome não informado: " })
+      }
+      if (!endereco) {
+        erros.push({ error: true, code: 400, message: "Endereço não informado: " })
+      }
+      if (!latitude) {
+        erros.push({ error: true, code: 400, message: "Latitude não informado: " })
+      }
+      if (!longitude) {
+        erros.push({ error: true, code: 400, message: "Longitude não informado: " })
+      }
+      if (!ip) {
+        erros.push({ error: true, code: 400, message: "Ip não informado: " })
+      }
+      if (!status) {
+        erros.push({ error: true, code: 400, message: "Status não informado: " })
+      }
+
+
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ messagemErro: error.message });
+    }
+  };
+
+
 }
 
 export default Estacao;
