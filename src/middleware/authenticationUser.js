57c49588a18baeca1ab6e59127b2175dfd8403f5
Kelve Oliveira <kelveoliveira052@gmail.com>
2024-04-11 19:39:51 -0400

    criação do middleware que verifica a autenticação

---
+++ src/middleware/authenticationUser.js
@@ -0,0 +1,20 @@
+import Jwt from "jsonwebtoken"
+
+
+const authentication = (req, res, next)=>{
+  const authHerades = req.headers.authorization
+  if(!authHerades || !authHerades.startsWith('Bearer ')){
+    res.status(400).json({Error: "Token not provided"})
+  }else{
+    const token = authHerades.split(' ')[1]
+    try {
+      Jwt.verify(token, process.env.JWT_SECRET)
+      next()
+    } catch (error) {
+      res.status(401).json({Error: "Incorret Token"})
+    }
+  }
+  next()
+}
+
+export default authentication;
