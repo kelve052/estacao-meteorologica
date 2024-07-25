9acd822b8e683fc83a35c791f8d0a63adf8571bd
Guilherme <guilhermekrauseramos@gmail.com>
2024-07-25 11:35:06 -0400

    Criação da rota para atualizar estação

--- src/routes/estacaoRouter.js
+++ src/routes/estacaoRouter.js
@@ -5,6 +5,7 @@ import autenticacaoUser from '../middleware/authenticationUser.js'
 const router = express.Router();
 router.get('/estacoes', autenticacaoUser, Estacao.listar);
 router.get('/estacoes/:id', Estacao.listarPorID);
+router.patch('/estacoes/:id', Estacao.editarEstacao);
 router.post('/cadastrarEstacao', Estacao.cadastrarEstacao)
 
 
