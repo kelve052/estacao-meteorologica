f3f8747df4c15fea0c3ca91f50ed1d526a968242
Kelve Oliveira <kelveoliveira052@gmail.com>
2024-07-30 22:22:16 -0400

    teste da rota de criação de token concluido

---
+++ src/tests/autenticacaoTest/IntegracaoAutenticacao.spec.js
@@ -0,0 +1,24 @@
+import request from "supertest";
+import { expect } from "@jest/globals";
+import app from "../../app.js";
+
+
+it('Login com autenticação jwt', async () => {
+  const response = await request(app)
+    .post("/autenticacao")
+    .send({
+      email: "frank@example.com",
+      senha: "senha131"
+    })
+    console.log(response)
+  expect(response.status).toBe(201)
+  expect(response.body.message).toBe("Token gerado com sucesso!");
+  expect(response.body.error).toBe(false);
+  expect(response.body.data).toMatchObject({
+    id: 10,
+    nome: 'Frank',
+    idade: 27,
+    email: 'frank@example.com',
+    senha: 'senha131'
+  })
+})
