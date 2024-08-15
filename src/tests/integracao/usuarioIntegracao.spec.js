ef822ece37faf876f16338143f856f36513ecbab
Lucas Silva <lucas12310@hotmail.com>
2024-08-14 22:08:19 -0400

    finalizando teste de integração

--- src/tests/integracao/usuarioIntegracao.spec.js
+++ src/tests/integracao/usuarioIntegracao.spec.js
@@ -25,49 +25,54 @@ it('Login com autenticação jwt', async () => {
 
 describe("Listar usuarios", () => {
     
-    it('Listagem de usuarios', async () => {
+    it('Deve retornar sucesso a listagem de usuarios', async () => {
         const response = await request(app)
             .get("/usuario")
             .set("Authorization", `Bearer ${token}`)
             .set("Content-Type", "application/json")
-
         const body = response.body;
-        console.log(body)
-        expect(body).toBeInstanceOf(object);
-        //idvalido=body.response[0].id;
+        idvalido = response.body[0].id;
+        //deve retornar status 200
+        expect(response.status).toBe(200);    
+        //deve retornar erro falso
+        expect({error: false}).toHaveProperty('error', false);
+        //testando se o corpo da requisição é um array
+        expect(body).toBeInstanceOf(Array);        
         //testando se retorna json
         expect(response.headers['content-type']).toContain('json');
-        expect(response.status).toBe(200);
-      });
+    });
     
-    // it('Listar usuario por ID valido', async () => {
-    //   const response = await request(app)
-    //       .get(`/usuario/${idvalido}`)
-    //       .set("Authorization", `Bearer ${token}`)
-    //       .set("Content-Type", "application/json")
-    //     //testando a resposta
-    //   expect(response.status).toBe(200);
-    //   // testando se esta retornando o id esperado
-    //   expect({id: idvalido}).toHaveProperty('id', idvalido);
-    //   //testando se retorna json
-    //   expect(response.headers['content-type']).toContain('json');
-    //   //testando a resposta response.body é uma instancia de um objeto
-    //   expect(response.body).toBeInstanceOf(Array);
-    // }); 
-  //   it('Deve retornar erro ao listar usuario com id invalido', async () => {
-  //     const idinvalido = "9999";
-  //     const response = await request(app)
-  //         .get(`/usuario/${idinvalido}`)
-  //         .set("Authorization", `Bearer ${token}`)
-  //         .set("Content-Type", "application/json")
-  //       //testando a resposta
-  //       expect(response.status).toBe(400);
-  //       //testando se retorna json
-  //       expect(response.headers['content-type']).toContain('json');
-  //       //testando se retorna o motivo do erro
-  //       expect({message: 'Estação não encontrada'}).toHaveProperty('message', "Estação não encontrada");
-  //       //testando se o erro esta ativo
-  //       expect({error: true}).toHaveProperty('error', true);
-  // });
+    it('Deve retornar sucesso ao listar usuario com ID valido', async () => {
+      const response = await request(app)
+          .get(`/usuario/${idvalido}`)
+          .set("Authorization", `Bearer ${token}`)
+          .set("Content-Type", "application/json")
+        const body = response.body;
+        //deve retornar a mensagem correta de sucesso
+        expect({message: 'Usuário encontrado com sucesso'}).toHaveProperty('message', "Usuário encontrado com sucesso");
+        //deve retornar status 200
+        expect(response.status).toBe(200);    
+        //deve retornar erro falso
+        expect({error: false}).toHaveProperty('error', false);
+        //testando se o corpo da requisição é um array
+        expect(body).toBeInstanceOf(Array);        
+        //testando se retorna json
+        expect(response.headers['content-type']).toContain('json');
+    }); 
+    it('Deve retornar sucesso ao listar usuario com ID invalido', async () => {
+      const idinvalido = "9999";
+      const response = await request(app)
+          .get(`/usuario/${idinvalido}`)
+          .set("Authorization", `Bearer ${token}`)
+          .set("Content-Type", "application/json")
+        //testando se retorna o motivo do erro
+        expect({message: 'Usuário não encontrado'}).toHaveProperty('message', "Usuário não encontrado");
+        //testando o status da resposta
+        expect(response.status).toBe(400);
+        //testando se o erro esta ativo
+        expect({error: true}).toHaveProperty('error', true);
+        //testando se retorna json
+        expect(response.headers['content-type']).toContain('json');
+  });
 });
 
