dd38df2854e800bc5599a8f642886eaa6dc0a9bc
Guilherme <guilhermekrauseramos@gmail.com>
2024-08-24 16:39:09 -0400

    Finalização das alterações na API

--- src/controllers/dadosController.js
+++ src/controllers/dadosController.js
@@ -3,49 +3,49 @@ import dadosService from "../services/dadosService.js";
 class Dados {
     static async listar(req, res) {
         try {
+            // Quando a API envia textplain;
+            // const jsonBody = JSON.parse(req.body);
             const { temperature, humidity, rainfall, wind_speed_kmh, data_hora } = req.query;
-
             const filtro = {
                 temperature: temperature,
                 humidity: humidity,
                 rainfall: rainfall,
                 wind_speed_kmh: wind_speed_kmh,
                 data_hora: data_hora,
-            }
-
+            };
             const response = await dadosService.listar(filtro)
-
-            res.status(200).json(response)
-
+            return res.status(200).json({
+                data: response,
+                error: false,
+                code: 200,
+                message: response.length > 1 ? "Dados climáticos encontrados com sucesso." : "Dado climático encontrado com sucesso.",
+            });
         } catch (error) {
-            res.status(error.code).json(error)
-        }
-    }
+            res.status(error.code || 500).json(error);
+        };
+    };
 
     static async inserir(req, res) {
         try {
             const { temperature, humidity, rainfall, wind_speed_kmh } = req.body;
-
             const data = {
                 temperature: temperature,
                 humidity: humidity,
                 rainfall: rainfall,
                 wind_speed_kmh: wind_speed_kmh,
                 data_hora: new Date(),
-            }
-
+            };
             const response = await dadosService.inserir(data)
-
             return res.status(201).json({
                 data: response,
-                message: 'Dados salvos com sucesso!',
+                error: false,
                 code: 201,
-                error: false
+                message: 'Dados climáticos salvos com sucesso.',
             });
         } catch (error) {
-            return res.status(error.code).json(error);
-        }
-    }
-}
+            return res.status(error.code || 500).json(error);
+        };
+    };
+};
 
-export default Dados
+export default Dados;
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,11 +12,28 @@ const routes = (app) => {
     });
     app.use(
         express.json(),
+        express.text(),
         estacoes,
         autenticacao,
         usuarios,
         dados
     );
+
+    // Uso apenas quando a estação manda text plain;
+
+    // app.use((req, res) => {
+    //     const textPlainBody = req.body;
+    //     const timestamp = new Date().toISOString();
+    //     let ip = req.headers["x-forwarded-for"] ||
+    //     req.socket.remoteAddress ||
+    //     null;
+    //     console.log(timestamp+" "+ip+" "+req.method+" "+req.protocol + "://" + req.get("host") + req.originalUrl);
+    //     console.log("-----------------------------")
+    //     console.log(textPlainBody);
+    //     console.log("-----------------------------")
+    //     console.log("")
+    //     res.send("OK");
+    // });
 };
 
 export default routes;
